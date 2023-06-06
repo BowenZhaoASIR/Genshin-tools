4c6fe37d8cbc4b3f543c4f7a18d4d524bf1886d1
Bowen Zhao <94955173+BowenZhaoASIR@users.noreply.github.com>
2023-06-06 19:36:28 +0200

    Update +server.js on hello for testing

--- src/routes/api/hello/+server.js
+++ src/routes/api/hello/+server.js
@@ -1,3 +1,20 @@
-export const GET = () => {
-    return new Response(JSON.stringify({message: "hello world"}), {status: 200 })
-}
+//---------------------------test----------------------------
+export const GET = async () => {
+  try {
+    const response = await fetch('https://api.genshin.dev/characters');
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return {
+      body: JSON.stringify(data),
+      status: 200,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      body: JSON.stringify({ message: 'Error' }),
+      status: 500,
+    };
+  }
+};
